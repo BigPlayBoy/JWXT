2956efd8875f3cdcf47c539ce95ac4f604e5ee2d
JARVIS <1151770629@qq.com>
2016-08-16 17:42:19 +0800

    增加测试

---
+++ src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import Bean.StudentEntity;
+
+/**
+ * Created by CUI on 2016/8/16.
+ */
+public class test{
+    public static void main(String[] args) {
+        StudentEntity studentEntity=new StudentEntity();
+        studentEntity.setName("123456");
+    }
+}
