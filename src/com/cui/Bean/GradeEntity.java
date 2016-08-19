3ca2379b0d8eeaa25b70347568e10f8bb99c2a17
JARVISTELLYES <1151770629@qq.com>
2016-08-19 16:58:57 +0800

    周五下班

--- src/com/cui/Bean/GradeEntity.java
+++ src/com/cui/Bean/GradeEntity.java
@@ -12,6 +12,65 @@ public class GradeEntity {
     private Double jidian;
     private String shuxing;
     private String time;
+    private StudentEntity studentEntity;
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GradeEntity that = (GradeEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (sid != null ? !sid.equals(that.sid) : that.sid != null) return false;
+        if (kecheng != null ? !kecheng.equals(that.kecheng) : that.kecheng != null) return false;
+        if (xuefen != null ? !xuefen.equals(that.xuefen) : that.xuefen != null) return false;
+        if (cehngji != null ? !cehngji.equals(that.cehngji) : that.cehngji != null) return false;
+        if (jidian != null ? !jidian.equals(that.jidian) : that.jidian != null) return false;
+        if (shuxing != null ? !shuxing.equals(that.shuxing) : that.shuxing != null) return false;
+        if (time != null ? !time.equals(that.time) : that.time != null) return false;
+        return studentEntity != null ? studentEntity.equals(that.studentEntity) : that.studentEntity == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (sid != null ? sid.hashCode() : 0);
+        result = 31 * result + (kecheng != null ? kecheng.hashCode() : 0);
+        result = 31 * result + (xuefen != null ? xuefen.hashCode() : 0);
+        result = 31 * result + (cehngji != null ? cehngji.hashCode() : 0);
+        result = 31 * result + (jidian != null ? jidian.hashCode() : 0);
+        result = 31 * result + (shuxing != null ? shuxing.hashCode() : 0);
+        result = 31 * result + (time != null ? time.hashCode() : 0);
+        result = 31 * result + (studentEntity != null ? studentEntity.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GradeEntity{" +
+                "id=" + id +
+                ", sid=" + sid +
+                ", kecheng='" + kecheng + '\'' +
+                ", xuefen=" + xuefen +
+                ", cehngji=" + cehngji +
+                ", jidian=" + jidian +
+                ", shuxing='" + shuxing + '\'' +
+                ", time='" + time + '\'' +
+                ", studentEntity=" + studentEntity +
+                '}';
+    }
+
+    public StudentEntity getStudentEntity() {
+        return studentEntity;
+    }
+
+    public void setStudentEntity(StudentEntity studentEntity) {
+        this.studentEntity = studentEntity;
+    }
 
     public Integer getId() {
         return id;
@@ -77,49 +136,4 @@ public class GradeEntity {
         this.time = time;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GradeEntity that = (GradeEntity) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (sid != null ? !sid.equals(that.sid) : that.sid != null) return false;
-        if (kecheng != null ? !kecheng.equals(that.kecheng) : that.kecheng != null) return false;
-        if (xuefen != null ? !xuefen.equals(that.xuefen) : that.xuefen != null) return false;
-        if (cehngji != null ? !cehngji.equals(that.cehngji) : that.cehngji != null) return false;
-        if (jidian != null ? !jidian.equals(that.jidian) : that.jidian != null) return false;
-        if (shuxing != null ? !shuxing.equals(that.shuxing) : that.shuxing != null) return false;
-        if (time != null ? !time.equals(that.time) : that.time != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (sid != null ? sid.hashCode() : 0);
-        result = 31 * result + (kecheng != null ? kecheng.hashCode() : 0);
-        result = 31 * result + (xuefen != null ? xuefen.hashCode() : 0);
-        result = 31 * result + (cehngji != null ? cehngji.hashCode() : 0);
-        result = 31 * result + (jidian != null ? jidian.hashCode() : 0);
-        result = 31 * result + (shuxing != null ? shuxing.hashCode() : 0);
-        result = 31 * result + (time != null ? time.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GradeEntity{" +
-                "id=" + id +
-                ", sid=" + sid +
-                ", kecheng='" + kecheng + '\'' +
-                ", xuefen=" + xuefen +
-                ", cehngji=" + cehngji +
-                ", jidian=" + jidian +
-                ", shuxing='" + shuxing + '\'' +
-                ", time='" + time + '\'' +
-                '}';
-    }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,10 @@
 package test;
 
+import com.cui.Bean.GradeEntity;
 import com.cui.Bean.StudentEntity;
 import com.cui.tools.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * Created by CUI on 2016/8/16.
@@ -9,17 +12,33 @@ import com.cui.tools.HibernateUtil;
 public class test {
 
     public static void main(String[] args) {
-        HibernateUtil.queryLogin();
-//        StudentEntity studentEntity = new StudentEntity();
-//        studentEntity.setName("张三");
-//        studentEntity.setSex("nan");
-//        studentEntity.setId(1330090002);
-//        studentEntity.setXuezhi("4");
-//        studentEntity.setYuanxi("计算计");
-//        studentEntity.setZhuanye("计算机");
-//        studentEntity.setBanji("1301");
-//        studentEntity.setRuxueriqi("123456");
-//        studentEntity.setGradeNumber(45);
+//        HibernateUtil.queryLogin();
+        StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setName("张三");
+        studentEntity.setSex("nan");
+        studentEntity.setId(1330090002);
+        studentEntity.setXuezhi("4");
+        studentEntity.setYuanxi("计算计");
+        studentEntity.setZhuanye("计算机");
+        studentEntity.setBanji("1301");
+        studentEntity.setRuxueriqi("123456");
+        studentEntity.setGradeNumber(45);
+        GradeEntity gradeEntity = new GradeEntity();
+        gradeEntity.setId(1);
+        gradeEntity.setKecheng("shuxue");
+        GradeEntity gradeEntity1 = new GradeEntity();
+        gradeEntity1.setId(2);
+        gradeEntity1.setKecheng("语文");
+        gradeEntity.setStudentEntity(studentEntity);
+        gradeEntity1.setStudentEntity(studentEntity);
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        session.save(studentEntity);
+        session.save(gradeEntity);
+        session.save(gradeEntity1);
+        tx.commit();
+        HibernateUtil.closeSession(session);
+//        studentEntity.
 //        addObject(studentEntity);
 //        addGrade();
 //        findStudentbyId();
@@ -28,6 +47,4 @@ public class test {
 //        gradeEntity.setKecheng("语文");
 //        updateStudent();
     }
-
-
 }
